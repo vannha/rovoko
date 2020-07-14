abd6df4e2ead98ef68cbc75c4fe1b3f06d0a9a22
vannha <phongnhatk21@gmail.com>
2020-07-14 14:10:26 +0700

    commit -

--- inc/demo-data.php
+++ inc/demo-data.php
@@ -5,7 +5,7 @@
 if ( ! function_exists( 'rovoko_enable_export_mode' ) ) {
 	add_filter( 'ef5_ie_export_mode', 'rovoko_enable_export_mode' );
 	function rovoko_enable_export_mode() {
-		return false;
+		return true;
 	}
 }
 /**
